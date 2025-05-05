a1d7dcd506b7cad3a2825a61eff6559fb30b68df
Abramov Vadim <abramov.vs@devzone.local>
2025-05-05 20:24:11 +0300

    added lang detection

--- client/src/app/i18n/index.ts
+++ client/src/app/i18n/index.ts
@@ -1,13 +1,16 @@
 import { initReactI18next } from 'react-i18next'
 import i18n from 'i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
 
 import * as resources from './locales'
 
 i18n
+  .use(LanguageDetector)
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources,
-    lng: 'ru', // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
+    fallbackLng: 'en',
+    // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
     // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
     // if you're using a language detector, do not define the lng option
 
