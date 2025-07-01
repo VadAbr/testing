61b93da14d868b0863cf4227a4dd136b68a83f7e
Abramov Vadim <abramov.vs@devzone.local>
2025-07-01 22:59:48 +0300

    added adaptive

--- client/src/pages/contacts/index.tsx
+++ client/src/pages/contacts/index.tsx
@@ -12,7 +12,7 @@ export const ContactsPage = () => {
   const { t } = useTranslation()
 
   return (
-    <PageContent>
+    <PageContent className={styles.container}>
       <div style={{ height: '100%' }} className={cnMixFlex({ align: 'center', justify: 'center' })}>
         <Card className={styles.card}>
           <Text size="3xl" align="center" weight="semibold" style={{ marginBottom: '2rem' }}>
